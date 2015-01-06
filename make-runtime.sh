f2c1fe92a4bdf372c7b6217d87ac5b28933effe4
Siavash Mirarab <smirarab@gmail.com>
2015-01-06 14:53:58 -0600

    update

--- make-runtime.sh
+++ make-runtime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-grep finish model.*/*/*.astral.err | sed -e "s/\// /g" -e "s/model.//g" -e "s/\./ /" -e "s/000\./K /" -e "s/000K /M /" -e "s/\.genes/ /" -e "s/.astral.err:.*in / /g"|sed -e "s/-halfresolved//g"| tee runtime.stat
-cat mrlruntime | sed -e "s/\// /g" -e "s/model.//g" -e "s/\./ /" -e "s/000\./K /" -e "s/000K /M /" -e "s/\.genes/ /" -e "s/.halfresolved//g" -e "s/$/ secs/" | tee -a runtime.stat
-cat greedytime | sed -e "s/\// /g" -e "s/model.//g" -e "s/\./ /" -e "s/000\./K /" -e "s/000K /M /" -e "s/\.genes/ /" -e "s/.halfresolved//g" -e "s/$/ secs/" | tee -a runtime.stat
-grep real model.*/*/*.time.stat| sed -e "s/\// /g" -e "s/model.//g" -e "s/\./ /" -e "s/000\./K /" -e "s/000K /M /" -e "s/\.genes/ /" -e "s/.time.stat:real / /g"|sed -e "s/.halfresolved//g"|tee -a runtime.stat
-grep Overall model.*/*/RAxML_info.mrl.*|sed -e "s/\// /g" -e "s/model.//g" -e "s/\./ /" -e "s/000\./K /" -e "s/000K /M /" -e "s/\.genes/ /" -e "s/ RAx.*mrl/ mrl/g" -e "s/:Over.*: / /" -e "s/ or.*//"|sed -e "s/.halfresolved//g" |tee -a runtime.stat
+grep finish model.*/*/*.astral.err | sed -e "s/\// /g" -e "s/model.//g" -e "s/\./ /" -e "s/000\./K /" -e "s/000K /M /" -e "s/\.genes\(.*\)-\(.*\)ls/ \1 \2/" -e "s/\.genes\(.[0-9]*\)\./ \1 -1 /" -e "s/.astral.err:.*in / /g"|sed -e "s/-halfresolved//g" > runtime.stat
+cat mrlruntime | sed -e "s/\// /g" -e "s/model.//g" -e "s/\./ /" -e "s/000\./K /" -e "s/000K /M /" -e "s/\.genes\(.*\)-\(.*\)ls/ \1 \2/" -e "s/\.genes\(.[0-9]*\) / \1 -1 /" -e "s/.halfresolved//g" -e "s/$/ secs/" >> runtime.stat 
+cat greedytime | sed -e "s/\// /g" -e "s/model.//g" -e "s/\./ /" -e "s/000\./K /" -e "s/000K /M /" -e "s/\.genes\(.*\)-\(.*\)ls/ \1 \2/" -e "s/\.genes\(.[0-9]*\) / \1 -1 /" -e "s/.halfresolved//g" -e "s/$/ secs/" >> runtime.stat 
+grep real model.*/*/*.time.stat| sed -e "s/\// /g" -e "s/model.//g" -e "s/\./ /" -e "s/000\./K /" -e "s/000K /M /" -e "s/\.genes\(.*\)-\(.*\)ls/ \1 \2/" -e "s/\.genes\(.[0-9]*\)\./ \1 -1 /" -e "s/.time.stat:real / /g"|sed -e "s/.halfresolved//g" >>  runtime.stat
+grep Overall model.*/*/RAxML_info.mrl.*|sed -e "s/\// /g" -e "s/model.//g" -e "s/\./ /" -e "s/000\./K /" -e "s/000K /M /" -e "s/\.genes\(.*\)-\(.*\)ls/ \1 \2/" -e "s/\.genes\(.[0-9]*\)./ \1 -1 /" -e "s/ RAx.*mrl/ mrl/g" -e "s/.Over.*: / /" -e "s/ or.*//"|sed -e "s/.halfresolved//g" >> runtime.stat
--- make-stat-st-err.sh
+++ make-stat-st-err.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for x in `ls */*/*.genes1000 */*/*.genes200 */*/*.genes50 |grep -v RAxML`; do 
+for x in `ls */*/*.genes1000 */*/*.genes200 */*/*.genes50 */*/*.genes*-*ls |grep -v RAxML`; do 
    test -s $x.score && continue; 
    echo -n $x" "; 
    test -s $x || continue; 
    echo `compareTrees.missingBranch ${x%/*}/s_tree.trees $x` `compareTrees.missingBranch $x ${x%/*}/s_tree.trees` |tee $x.score; 
 done
 
-grep " " */*/*.score |sed -e "s/\// /g" -e "s/model.//g" -e"s/\./ /" -e "s/000\./K /" -e "s/000K /M /" -e "s/\.genes/ /" -e "s/.score:/ /g"| sed -e "s/.halfresolved//g" > scores.stat
+grep " " */*/*.score |sed -e "s/\// /g" -e "s/model.//g" -e"s/\./ /" -e "s/000\./K /" -e "s/000K /M /" -e "s/\.genes\(.*\)-\(.*\)ls/ \1 \2/" -e "s/\.genes\(.[0-9]*\)\./ \1 -1 /" -e "s/.score:/ /g"| sed -e "s/.halfresolved//g" > scores.stat
