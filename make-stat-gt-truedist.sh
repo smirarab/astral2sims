c12d4d06037d2d3419e84c8e389041597a9ace81
Siavash Mirarab <smirarab@gmail.com>
2015-04-02 12:14:27 -0500

    update

--- make-stat-gt-truedist.sh
+++ make-stat-gt-truedist.sh
@@ -2,7 +2,7 @@
 
 p=`pwd`;
 
-for x in main.2*/01/; do cd $p/$x && pwd && compareTrees.missingBranch s_tree.trees truegenetrees|tee gt-truedist.stat; done 
+for x in model.*/$1/; do cd $p/$x && pwd && compareTrees.missingBranch s_tree.trees truegenetrees|tee gt-truedist.stat; done 
 
-grep " " */*/gt-truedist.stat|sed -e "s/.gt.*:/ /g" -e "s/main.//g" -e "s/\./ /" -e "s/\// /g" -e "s/000//" -e "s/ /k /"|tee gt-truedsit.stat
+grep " " */*/gt-truedist.stat|sed -e "s/.gt.*:/ /g" -e "s/model.//g" -e "s/\./ /" -e "s/\// /g" -e "s/000//" -e "s/ /k /"|tee gt-truedist.stat
 
